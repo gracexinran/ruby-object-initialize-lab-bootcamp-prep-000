2de7f67028cee47b933ab73fc7a38efc7d19d6e8
Xinran Che <gracexinran@gmail.com>
2019-04-30 01:23:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-
-  def name
-    @nmae
-  end
-  def breed
-    @breed
   end
 end
